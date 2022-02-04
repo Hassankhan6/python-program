1b8177777b47187970d0592d2a8e249156d8196a
Hassan khan <hassan33.hk98@gmail.com>
2022-02-04 13:03:46 +0500

    <your message >

---
+++ Assignment 1/C__Users_THE LAPTOP STORE_Desktop.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("HEllo World");
+
+
+# In[2]:
+
+
+a=5;
+b=3;
+c=a+b;
+print(c);
+
+
+# In[4]:
+
+
+f="8";
+g="7";
+h=f+g;
+print(h);
+
+
+# In[8]:
+
+
+x="hassan ";
+y="khan";
+z=x+y;
+print(z);
+
+
+# In[11]:
+
+
+x=8;
+y=x+4;
+z=y-x+4;
+print(z);
+
+
+# In[14]:
+
+
+a=12-4/2+9*5
+print(a)
+
+
+# In[15]:
+
+
+a= ((12- 4)/ (2 +9)* 5);
+print(a);
+
+
+# In[16]:
+
+
+x=10;
+y=x%3;
+print(y);
+
+
+# In[18]:
+
+
+f1=4;
+f=5;
+print(f1);
+
+
+# In[19]:
+
+
+g =5;
+g= g+2;
+print(g);
+
+
+# In[23]:
+
+
+a = 3;
+a+=3;
+print(a);
+
+
+# In[27]:
+
+
+a=3;
+a = a+3;
+print(a);
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
